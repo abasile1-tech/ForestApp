b12cec4e9de7c89cfea48d35ff2f2bb0b3cdfe58
John <ashton.basile@gmail.com>
2023-09-18 14:41:03 +0100

    I added some before setup

--- src/test/java/BearTest.java
+++ src/test/java/BearTest.java
@@ -1,11 +1,23 @@
 import com.codebase.Bear;
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 
 public class BearTest {
+    Bear bear;
+
+    @Before
+    public void before() {
+        bear = new Bear("Baloo", 20, 523.12);
+    }
+
     @Test
     public void hasName() {
-        Bear bear = new Bear("Baloo", 20, 523.12);
         assertEquals("Baloo", bear.getName());
+    }
+
+    @Test
+    public void hasAge() {
+        assertEquals(20, bear.getAge());
     }
 }
